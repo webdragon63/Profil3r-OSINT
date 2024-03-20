97506e79a6f139a96ea4e6145fd4f4b3cf35dca5
webdragon63 <117004849+webdragon63@users.noreply.github.com>
2024-03-20 14:46:13 +0000

    Update twitter.py

--- profil3r/modules/social/twitter.py
+++ profil3r/modules/social/twitter.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from profil3r.colors import Colors
 import time
 
 class Twitter:
@@ -14,16 +14,13 @@ class Twitter:
         # You can find more at https://github.com/zedeus/nitter/wiki/Instances
         self.nitter_URL = [
             "https://nitter.42l.fr/{}",
-            "https://nitter.pussthecat.org/{}",
-            "https://nitter.nixnet.services/{}",
-            "https://nitter.tedomum.net/{}",
-            "https://nitter.fdn.fr/{}",
-            "https://nitter.kavin.rocks/{}",
-            "https://tweet.lambda.dance/{}"
+            "nitter.pussthecat.org/{}",
+            "nitter.nixnet.services/{}",
+            "nitter.tedomum.net/{}",
+            "nitter.fdn.fr/{}",
+            "nitter.kavin.rocks/{}",
+            "tweet.lambda.dance/{}"
         ]
-
-        #social
-        self.type = config['plateform']['twitter']['type']
 
     # Generate all potential twitter usernames
     def possible_usernames(self):
@@ -44,7 +41,6 @@ class Twitter:
 
     def search(self):
         twitter_usernames = {
-            "type": self.type,
             "accounts" : []
         }
 
@@ -61,37 +57,7 @@ class Twitter:
             
             # If the account exists
             if r.status_code == 200:
-                # Account object
-                account = {}
-
-                # Get the username
-                account["value"] = username
-                
-                # Parse HTML response content with beautiful soup 
-                soup = BeautifulSoup(r.text, 'html.parser')
-                
-                # Scrape the user informations
-                try:
-                    user_full_name = str(soup.find_all(class_='profile-card-fullname')[0].get_text()).strip() if soup.find_all(class_='profile-card-fullname') else None
-                    user_username = str(soup.find_all(class_='profile-card-username')[0].get_text()).strip() if soup.find_all(class_='profile-card-username') else None
-                    user_bio = str(soup.find_all(class_='profile-bio')[0].get_text()).replace("\n", "").strip() if soup.find_all(class_='profile-bio') else None
-                    user_tweets_count = str(soup.find_all(class_='profile-stat-num')[0].get_text().replace(",", "")).strip() if soup.find_all(class_='profile-stat-num') else None
-                    user_following_count = str(soup.find_all(class_='profile-stat-num')[1].get_text().replace(",", "")) if soup.find_all(class_='profile-stat-num') else None
-                    user_followers_count = str(soup.find_all(class_='profile-stat-num')[2].get_text().replace(",", "")).strip() if soup.find_all(class_='profile-stat-num') else None
-                    user_likes_count = str(soup.find_all(class_='profile-stat-num')[3].get_text().replace(",", "")).strip() if soup.find_all(class_='profile-stat-num') else None
-
-                    account["full_name"] = {"name": "Full Name", "value": user_full_name}
-                    account["username"] = {"name": "Username", "value": user_username}
-                    account["bio"] = {"name": "Bio", "value": user_bio}
-                    account["tweets_count"] = {"name": "Tweets", "value": user_tweets_count}
-                    account["following_count"] = {"name": "Following", "value": user_following_count}
-                    account["followers_count"] = {"name": "Followers", "value": user_followers_count}
-                    account["likes_count"] = {"name": "Likes", "value": user_likes_count}
-                except:
-                    pass
-                
-                # Append the account to the accounts table
-                twitter_usernames["accounts"].append(account)
+                twitter_usernames["accounts"].append({"value": username})
 
             time.sleep(self.delay)
 
