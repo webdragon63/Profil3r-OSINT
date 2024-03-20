26978163180a61497f912273c8965bda6d124f65
webdragon63 <117004849+webdragon63@users.noreply.github.com>
2024-03-20 14:45:20 +0000

    Update instagram.py

--- profil3r/modules/social/instagram.py
+++ profil3r/modules/social/instagram.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from profil3r.colors import Colors
 import time
 
 class Instagram:
@@ -10,8 +10,6 @@ class Instagram:
         # https://instagram.com/{username}
         self.format = config['plateform']['instagram']['format']
         self.permutations_list = permutations_list
-        # social
-        self.type = config['plateform']['instagram']['type']
 
     # Generate all potential instagram usernames
     def possible_usernames(self):
@@ -25,7 +23,6 @@ class Instagram:
 
     def search(self):
         instagram_usernames = {
-            "type": self.type,
             "accounts" : []
         }
 
@@ -42,35 +39,7 @@ class Instagram:
             
             # If the account exists
             if r.status_code == 200:
-                # Account object
-                account = {}
-
-                # Get the username
-                account["value"] = username
-                
-                # Parse HTML response content with beautiful soup 
-                soup = BeautifulSoup(r.text, 'html.parser')
-                
-                # Scrape the user informations
-                try:
-                    user_full_name = str(soup.find_all(class_='full-name')[0].get_text()).strip()
-                    user_username = str(soup.find_all(class_='username')[0].get_text()).strip()
-                    user_bio = str(soup.find_all(class_='bio')[0].get_text()).replace("\n", "").strip()
-                    user_posts_count = str(soup.find_all(class_='count')[0].get_text().replace(",", "")).strip()
-                    user_following_count = str(soup.find_all(class_='count')[1].get_text().replace(",", "")).strip()
-                    user_followers_count = str(soup.find_all(class_='count')[2].get_text().replace(",", "")).strip()
-
-                    account["full_name"] = {"name": "Full Name", "value": user_full_name}
-                    account["username"] = {"name": "Username", "value": user_username}
-                    account["bio"] = {"name": "Bio", "value": user_bio}
-                    account["posts_count"] = {"name": "Posts", "value": user_posts_count}
-                    account["following_count"] = {"name": "Following", "value": user_following_count}
-                    account["followers_count"] = {"name": "Followers", "value": user_followers_count}
-                except:
-                    pass
-                
-                # Append the account to the accounts table
-                instagram_usernames["accounts"].append(account)
+                instagram_usernames["accounts"].append({"value": username})
 
             time.sleep(self.delay)
         
